bb3a08430d983a134c2a66b0f85e1252e41b3b62
François <Franois@192.168.1.38>
2019-03-04 23:46:18 +0100

    Changing home page

--- WebPortal/application/controllers/Welcome.php
+++ WebPortal/application/controllers/Welcome.php
@@ -20,6 +20,11 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+	    //Old Message
+		//$this->load->view('welcome_message');
+		
+	    //New message
+	    $this->load->helper('url');
+		$this->load->view('home');
 	}
 }
