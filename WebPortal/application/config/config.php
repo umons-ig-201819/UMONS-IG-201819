171a2fca8eebf29852eedfcee9971c4e0471697b
François <Franois@192.168.1.38>
2019-03-04 23:32:53 +0100

    Changement du chemin pour être compatible avec Wamp

--- WebPortal/application/config/config.php
+++ WebPortal/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://umons-ig-201819/';
+$config['base_url'] = 'http://localhost/UMONS-IG-201819/WebPortal/';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://umons-ig-201819/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = "index.php";
 
 /*
 |--------------------------------------------------------------------------
@@ -64,7 +64,7 @@ $config['uri_protocol']	= 'REQUEST_URI';
 |
 | https://codeigniter.com/user_guide/general/urls.html
 */
-$config['url_suffix'] = '';
+$config['url_suffix'] = "";
 
 /*
 |--------------------------------------------------------------------------
