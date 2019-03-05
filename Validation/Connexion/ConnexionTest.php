f57fb4b544d3764c0b1d5515f2a1f9bedc6bb821
aureliecools <aureliecools@10.37.129.2>
2019-03-05 12:01:23 +0100

    test init

---
+++ Validation/Connexion/ConnexionTest.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * Connexion test case.
+ */
+class ConnexionTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     *
+     * @var Connexion
+     */
+    private $connexion;
+
+    /**
+     * Prepares the environment before running a test.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        // TODO Auto-generated ConnexionTest::setUp()
+
+        $this->connexion = new Connexion(/* parameters */);
+    }
+
+    /**
+     * Cleans up the environment after running a test.
+     */
+    protected function tearDown()
+    {
+        // TODO Auto-generated ConnexionTest::tearDown()
+        $this->connexion = null;
+
+        parent::tearDown();
+    }
+
+    /**
+     * Constructs the test case.
+     */
+    public function __construct()
+    {
+        // TODO Auto-generated constructor
+    }
+}
+
