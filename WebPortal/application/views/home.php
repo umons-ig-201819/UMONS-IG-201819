357c4bdd9f94153c47b9dd88a3bb362716ed1796
François <Franois@192.168.1.38>
2019-03-08 23:32:09 +0100

    Création du header de la main page

--- WebPortal/application/views/home.php
+++ WebPortal/application/views/home.php
@@ -5,8 +5,21 @@
 	<title>Portail AWE</title>
 </head>
 <body>
+	<header>
+		<div id="logo">
+			<img src="images/LogoPrincipal" alt="Logo Principal" />               
+		</div>
+		<h1>Bienvenue sur le portail de l'AWE</h1>		
 
-<h1>Bienvenue sur le portail de l'AWE</h1>
+		<nav>
+			<ul>
+				<li><a href="#">Accueil</a></li>
+				<li><a href="#">Help</a></li>
+			</ul>
+		</nav>
+		<button class="topButtonStyle" type="button">Connexion</button>
+	</header>
+
 <p>This is the first paragraph</p>
 
 </body>
