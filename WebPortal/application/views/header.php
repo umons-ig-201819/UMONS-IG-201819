1dea704a3baad9b327e7aeb6c7c7c73086b7ebd0
Courgax <47362640+Courgax@users.noreply.github.com>
2019-03-10 20:29:50 +0100

    Un peu de fun sur le CSS

--- WebPortal/application/views/header.php
+++ WebPortal/application/views/header.php
@@ -8,24 +8,28 @@
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/javascript/home.js"></script>
 <title>Portail AWE</title>
 </head>
-<body>
-<header>
-<div id="logo">
-<img src="<?php echo base_url(); ?>assets/images/logo_awe_asbl.png" alt="Logo Principal AWE!" />
-<img src="<?php echo base_url(); ?>assets/images/top_commercial.png" alt="top commercial!" name="commercial" />
-</div>
-<h1> Portail de l'association wallonne de l'&eacute;levage </h1></br>
 
-	<nav>
-        <article class="topnav"> 
-            <a href="<?php echo base_url("home/main"); ?>">Accueil</a>
-            <a href="<?php echo base_url("home/help"); ?>">Help</a>
-            <a href="<?php echo base_url("home/connect"); ?>">Connexion</a>
-            <a href="<?php echo base_url("home/inscript"); ?>">Inscription</a>
-        </article>
-        </br></br>
-	</nav>
- 
-</header>
+<body>
+    <header>
+        <div id="titre_principal">
+            <div id="logo">
+            	<img src="<?php echo base_url(); ?>assets/images/logo_awe_asbl.png" alt="Logo Principal AWE!" />
+            	<h1> Portail de l'association wallonne de l'&eacute;levage </h1>
+            </div>
+            	<h2> Page principale</h2>
+            	 <img src="<?php echo base_url(); ?>assets/images/top_commercial.png" alt="top commercial!" name="commercial" />
+        </div>    
+    
+    	<nav>
+            <article class="topnav"> 
+                <a href="<?php echo base_url("home/main"); ?>">Accueil</a>
+                <a href="<?php echo base_url("home/help"); ?>">Help</a>
+                <a href="<?php echo base_url("home/connect"); ?>">Connexion</a>
+                <a href="<?php echo base_url("home/inscript"); ?>">Inscription</a>
+            </article>
+            </br></br>
+    	</nav>
+     
+    </header>
 </body>
 </html>
--- WebPortal/application/views/home.php
+++ WebPortal/application/views/home.php
@@ -21,18 +21,21 @@
 				<img src="tofill.png" alt="IMAGE " />Actualit&eacute;s
 			</h3>
 		</article>
-		<article>
-			<h3>
-				<img src="tofill.png" alt="IMAGE " />G&eacute;n&eacute;ralit&eacute;s
-			</h3>
-		</article>
-
 		<aside>
 			<h3>Nombre d'agriculteurs inscrits</h3>
 
 			<h3>Nombre de visites par jour</h3>
 		</aside>
 	</section>
+	<section>
+		<article>
+			<h3>
+				<img src="tofill.png" alt="IMAGE " />G&eacute;n&eacute;ralit&eacute;s
+			</h3>
+		</article>
+	</section>
+
+
 	
 
 
