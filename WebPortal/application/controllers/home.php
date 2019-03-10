caef977cca9ecf6521c6545bcb14369e0ec546e2
Courgax <47362640+Courgax@users.noreply.github.com>
2019-03-10 16:07:51 +0100

    Ajout fonction par défaut du controleur Home

--- WebPortal/application/controllers/home.php
+++ WebPortal/application/controllers/home.php
@@ -5,9 +5,15 @@ class Home extends CI_Controller
     public function main()
     {
         //Loading url helper
-        $this->load->helper('url');
-        
+        $this->load->helper('url');        
         $this->load->view('home');
     }
+    public function index()
+    {        
+        echo 'Aucune méthode dans le controleur home pour cette page';        
+    }
+    
+    
+    
 }
 ?>
