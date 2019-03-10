686ec498d33db006a49b2d0abbf57431da37ac23
EmiGenin <em.genin@gmail.com>
2019-03-10 15:50:04 +0100

    header-footer-db-home.php

---
+++ WebPortal/application/views/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
+<title>Portail AWE</title>
+</head>
+
+    <body>
+<p>...bas de page...</p>
+    </body>
+
+</html>
---
+++ WebPortal/application/views/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="utf-8" />
+<link rel="stylesheet" type="text/css"
+	href="<?php echo base_url(); ?>assets/css/style.css">
+<title>Portail AWE</title>
+</head>
+<body>
+<header>
+<div id="logo">
+<img src="images/LogoPrincipal" alt="Logo Principal AWE!" />
+</div>
+<h1> Portail de l'association allonne de l'&eacute;levage </h1></br>
+
+	<nav>
+        <article class="topnav"> 
+            <a href="home.php">Accueil</a>
+            <a href="#">Help</a>
+            <a href="#">Connexion</a>
+            <a href="#">Inscription</a>
+        </article>
+        </br></br>
+	</nav>
+ 
+</header>
+</body>
+</html>
