03698b683a98c632fa5b2304de83b4a993f51d13
Jean-Sébastien Lerat <belegkarnil@10.0.0.125>
2019-03-17 06:57:20 +0100

    Change config to be comptatible with dev env and servers + header & footer views refactoring

--- WebPortal/application/views/footer.php
+++ WebPortal/application/views/footer.php
@@ -1,15 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-
-<meta charset="utf-8" />
-<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css">
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/javascript/home.js"></script>
-<title>Portail AWE</title>
-</head>
-
-    <body>
-<p>...bas de page...</p>
-    </body>
-
+	<footer></footer>
+	</body>
 </html>
--- WebPortal/application/views/header.php
+++ WebPortal/application/views/header.php
@@ -1,34 +1,29 @@
 <!DOCTYPE html>
 <html>
-<head>
-
-<meta charset="utf-8" />
-<link rel="stylesheet" type="text/css"
-	href="<?php echo base_url(); ?>assets/css/style.css">
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/javascript/home.js"></script>
-<title>Portail AWE</title>
-</head>
-
-<body>
-    <header>
-        <div id="titre_principal">
-            <div id="logo">
-            	<img src="<?php echo base_url(); ?>assets/images/logo_awe_asbl.png" alt="Logo Principal AWE!" />
-            	<h1> Portail de l'association wallonne de l'&eacute;levage </h1>
+    <head>
+    	<meta charset="UTF-8">
+    	<title>Portail AWE</title>
+    	<!--[if lt IE 9]>
+    		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    	<![endif]-->
+    	<link rel="stylesheet" type="text/css" media="only screen" href="<?=site_url('css/styles.css');?>" />
+    </head>
+    <body>
+    	<header id="main_header">
+            <div id="titre_principal">
+                <div id="logo">
+                	<img src="<?php echo base_url(); ?>assets/images/logo_awe_asbl.png" alt="Logo Principal AWE!" />
+                	<h1> Portail de l'association wallonne de l'&eacute;levage </h1>
+                </div>
+                	<h2> Page principale</h2>
+                	 <img src="<?php echo base_url(); ?>assets/images/top_commercial.png" alt="top commercial!" name="commercial" />
             </div>
-            	<h2> Page principale</h2>
-            	 <img src="<?php echo base_url(); ?>assets/images/top_commercial.png" alt="top commercial!" name="commercial" />
-        </div>    
-    
-    	<nav>
-            <ul> 
-                <li><a href="<?php echo base_url("home/main"); ?>">Accueil</a></li>
-                <li><a href="<?php echo base_url("home/help"); ?>">Help</a></li>
-                <li><a href="<?php echo base_url("home/connect"); ?>">Connexion</a></li>
-                <li><a href="<?php echo base_url("home/inscript"); ?>">Inscription</a></li>
-            </ul>
-    	</nav>
-     
-    </header>
-</body>
-</html>
+    		<nav>
+    			<ul>
+    			<li><a href="<?php echo base_url("home/main"); ?>">Accueil</a></li>
+                    <li><a href="<?php echo base_url("home/help"); ?>">Help</a></li>
+                    <li><a href="<?php echo base_url("home/connect"); ?>">Connexion</a></li>
+                    <li><a href="<?php echo base_url("home/inscript"); ?>">Inscription</a></li>
+    			</ul>
+    		</nav>
+    	</header>
