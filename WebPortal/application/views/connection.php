dbd8ffa9c40264d547887022708385ddd0f9ee32
Jean-Sébastien Lerat <belegkarnil@10.0.0.125>
2019-03-17 08:04:55 +0100

    update connection view (form extra parameters)

--- WebPortal/application/views/connection.php
+++ WebPortal/application/views/connection.php
@@ -2,17 +2,15 @@
 	<article>
 		<h1>Connexion</h1>
 		<?php
-$required = array('required' => 'required');
-
 echo form_open('connection');
 echo form_fieldset('Informations de connexion&nbsp;:');
 echo '<p>';
     echo form_label('Identifiant','username');
-    echo form_input('username',$this->input->post('username',TRUE) ,$required,'id="username"');
+    echo form_input('username',$this->input->post('username',TRUE),'id="username" required="required"');
 echo '</p>';
 echo '<p>';
     echo form_label('Mot de passe','password');
-    echo form_password('password',$required,'id="password"');
+    echo form_password('password','','id="password" required="required"');
 echo '</p>';
 echo form_submit('action', 'Se connecter');
 echo form_fieldset_close();
