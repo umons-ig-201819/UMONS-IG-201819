ba87ef8c715d73f8baaf4e4ead6f003477c412dc
Jean-Sébastien Lerat <belegkarnil@localhost.belegkarnil-shs>
2019-03-24 09:25:14 +0100

    Add configuration file to tests

---
+++ Validation/Connexion/configuration.php
@@ -0,0 +1,16 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+define("__ROOT_DIR__",dirname(__DIR__));
+define("__VALIDATION_DIR__",__ROOT_DIR__.'/Validation');
+define("__WEB_DIR__",__ROOT_DIR__.'/WebPortal');
+
+function load_model($name){
+    require_once(__WEB_DIR__.'/application/models/UserModel.php');
+}
+
+function load_controller($name){
+    require_once(__WEB_DIR__.'/application/controllers/UserModel.php');
+}
