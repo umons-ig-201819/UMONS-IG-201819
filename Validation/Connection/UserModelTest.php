ad8da2f92c963fc43f648bbde1395e49a78a203f
ACOOLS <47692726+ACOOLS@users.noreply.github.com>
2019-03-24 12:36:00 +0100

    new
    new

--- Validation/Connection/UserModelTest.php
+++ Validation/Connection/UserModelTest.php
@@ -3,6 +3,7 @@
 // use PHPUnit\Framework\TestCase;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\DbUnit\TestCaseTrait;
 
 require_once(__DIR__.'/../configuration.php');
 
@@ -10,6 +11,26 @@ load_model('UserModel');
 
 class ConnectionTest extends TestCase
 {
+    
+    use TestCaseTrait;
+
+    // only instantiate pdo once for test clean-up/fixture load
+    static private $pdo = null;
+
+    // only instantiate PHPUnit_Extensions_Database_DB_IDatabaseConnection once per test
+    private $conn = null;
+
+    final public function getConnection()
+    {
+        if ($this->conn === null) {
+            if (self::$pdo == null) {
+                self::$pdo = new PDO( $GLOBALS['DB_DSN'], $GLOBALS['DB_USER'], $GLOBALS['DB_PASSWD'] );
+            }
+            $this->conn = $this->createDefaultDBConnection(self::$pdo, $GLOBALS['DB_DBNAME']);
+        }
+
+        return $this->conn;
+    }
     
     /**
      *
