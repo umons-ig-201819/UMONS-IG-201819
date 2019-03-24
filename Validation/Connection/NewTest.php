30149311c7840a2dd87d9cd19f1f75a1133dff62
ACOOLS <47692726+ACOOLS@users.noreply.github.com>
2019-03-24 15:34:53 +0100

    test
    test

---
+++ Validation/Connection/NewTest.php
@@ -0,0 +1,39 @@
+
+
+class BookMapperTest extends DatabaseTestCase
+{
+    /**
+     * Prepare data set for database tests
+     * 
+     * @return \PHPUnit_Extensions_Database_DataSet_ArrayDataSet
+     */
+    public function getDataSet()
+    {
+        return $this->createArrayDataSet( array() );
+    }    
+    
+    public function testFetchAll()
+    {
+        $this->markTestIncomplete( 'Not written yet.' );
+    }
+
+    public function testFetchByISBN()
+    {
+        $this->markTestIncomplete( 'Not written yet.' );
+    }
+
+    public function testInsert()
+    {
+        $this->markTestIncomplete( 'Not written yet.' );
+    }
+
+    public function testUpdate()
+    {
+        $this->markTestIncomplete( 'Not written yet.' );
+    }
+
+    public function testDelete()
+    {
+        $this->markTestIncomplete( 'Not written yet.' );
+    }
+}
