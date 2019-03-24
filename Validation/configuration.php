41d67a1aedd334d11a3b0785806a417f56fb1ba5
Jean-Sébastien Lerat <belegkarnil@localhost.belegkarnil-shs>
2019-03-24 20:52:01 +0100

    t

--- Validation/configuration.php
+++ Validation/configuration.php
@@ -31,9 +31,9 @@ try{
 }catch(Exception $e){
 }catch(Error $ee){}
 
-echo "Loading ci_controller instance:\n"
+echo "Loading ci_controller instance:\n";
 echo CI_Controller::get_instance();
-echo "done\n"
+echo "done\n";
 
 require_once(BASEPATH."core/Model.php");
 require_once(BASEPATH."core/Controller.php");
