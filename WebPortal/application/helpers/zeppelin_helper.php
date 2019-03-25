ac5c95f048a0c99a052713c9c1563ed65de9b80c
Jean-Sébastien Lerat <belegkarnil@10.0.0.125>
2019-03-25 21:15:44 +0100

    synchronize_workspace@ZeppelinHelper: bug fix

--- WebPortal/application/helpers/zeppelin_helper.php
+++ WebPortal/application/helpers/zeppelin_helper.php
@@ -148,6 +148,7 @@ print_r($assoc);
             }else{
                 $assoc[$paragraph['origin']]['process'] = true;
                 $original       = $assoc[$paragraph['origin']];
+                print_r($original);
                 $originDate     = empty($original['dateStarted']) ? $original['dateCreated'] : $original['dateStarted'];
                 $paragraphDate  = empty($paragraph['dateStarted']) ? $paragraph['dateCreated'] : $paragraph['dateStarted'];
                 $originDate     = intval($originDate);
