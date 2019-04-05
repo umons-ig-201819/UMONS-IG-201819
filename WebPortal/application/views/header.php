fecc12c8bfa57396b9e56bbb69b98c76b6642268
fro <fro@H17-5CG7243S79.bbri.be>
2019-04-05 13:33:54 +0200

    CSS experiments (Oui c'est très laid)

--- WebPortal/application/views/header.php
+++ WebPortal/application/views/header.php
@@ -15,18 +15,19 @@
     	</style>
     </head>
     <body>
-    	<header id="main_header">
+    	<header id="main_header">    	 
             <div id="titre_principal">
                 <div id="logo">
                 	<img src="<?php echo base_url(); ?>assets/images/logo_awe_asbl.png" alt="Logo Principal AWE!" />
                 	<h1> Portail de l'association wallonne de l'&eacute;levage </h1>
                 </div>
-                	<h2> Page principale</h2>
-                	 <img src="<?php echo base_url(); ?>assets/images/top_commercial.png" alt="top commercial!" name="commercial" />
+                	
+                	 
  <!--           </div>     -->
     		<nav>
+    		<h2> Page principale</h2>
     			<ul>
-    			<li><a href="<?php echo site_url("home"); ?>">Accueil</a></li>
+    				<li><a href="<?php echo site_url("home"); ?>">Accueil</a></li>
                     <li><a href="<?php echo site_url("help"); ?>">Help</a></li>
                     <?php if(isset($this->session->UserID)): ?>
                     <li><a href="<?php echo site_url("connection/logout"); ?>">D&eacute;connexion</a></li>
