23ccfe63fc313cccb74fd0bc039a0b7c09ce0b29
Courgax <47362640+Courgax@users.noreply.github.com>
2019-04-06 12:11:38 +0200

    CSS work

--- WebPortal/application/views/header.php
+++ WebPortal/application/views/header.php
@@ -6,6 +6,7 @@
     	<!--[if lt IE 9]>
     		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     	<![endif]-->
+    	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> <!--  Ajout de la largeur de l'écran pour le dév-->
     	<link rel="stylesheet" type="text/css" media="only screen" href="<?php echo base_url(); ?>assets/css/style.css" />
     	<style type="text/css">
         	.alert-warning {
@@ -15,30 +16,30 @@
     	</style>
     </head>
     <body>
-    	<header id="main_header">    	 
-            <div id="titre_principal">
+    	<div id="bloc_page"> <!-- container page entière -->
+    	<header>         
                 <div id="logo">
                 	<img src="<?php echo base_url(); ?>assets/images/logo_awe_asbl.png" alt="Logo Principal AWE!" />
                 	<h1> Portail de l'association wallonne de l'&eacute;levage </h1>
-                </div>
-                	
-                	 
- <!--           </div>     -->
-    		<nav>
-    		<h2> Page principale</h2>
-    			<ul>
-    				<li><a href="<?php echo site_url("home"); ?>">Accueil</a></li>
-                    <li><a href="<?php echo site_url("help"); ?>">Help</a></li>
-                    <?php if(isset($this->session->UserID)): ?>
-                    <li><a href="<?php echo site_url("connection/logout"); ?>">D&eacute;connexion</a></li>
-                    <li><a href="<?php echo site_url("profil"); ?>">Mon profil</a></li>
-                    <li><a href="<?php echo site_url("datasource"); ?>">Mes donn&eacute;es/applications</a></li>
-                    <?php else: ?>
-                    <li><a href="<?php echo site_url("connection"); ?>">Connexion</a></li>
-                    <?php endif; ?>
-                    <li><a href="<?php echo site_url("register"); ?>">Inscription</a></li>
-              <!--     <li><a href="?php echo site_url("connection/register"); ?>">Inscription</a></li>        -->              
-    			</ul>
-    		</nav>
-    		</div>
+            		<nav>    		
+            			<ul>
+            				<li><a href="<?php echo site_url("home"); ?>">Accueil</a></li>
+                            <li><a href="<?php echo site_url("help"); ?>">Help</a></li>
+                            <?php if(isset($this->session->UserID)): ?>
+                            <li><a href="<?php echo site_url("connection/logout"); ?>">D&eacute;connexion</a></li>
+                            <li><a href="<?php echo site_url("profil"); ?>">Mon profil</a></li>
+                            <li><a href="<?php echo site_url("datasource"); ?>">Mes donn&eacute;es/applications</a></li>
+                            <?php else: ?>
+                            <li><a href="<?php echo site_url("connection"); ?>">Connexion</a></li>
+                            <?php endif; ?>
+                            <li><a href="<?php echo site_url("register"); ?>">Inscription</a></li>
+                      <!--     <li><a href="?php echo site_url("connection/register"); ?>">Inscription</a></li>        -->              
+            			</ul>
+            		</nav>
+    			</div>
+    			<h2> Page principale</h2>		
+    			
+    <!--  Ajout de la largeur de l'écran pour le dév-->
+    <div style="background:pink;color:#333;position:fixed;right:0;bottom:0;z-index:99999999;font:1em arial;opacity:.9" id="ld"></div><script>setInterval(function(){if($(window).height()>=$(document).height()){$('#ld').text($(document).width()+' px');}else{$('#ld').text($(document).width()+17+' px');}},150);
+</script>
     	</header>
