af005b3972777bd2041b13f25951af0d0414e738
Courgax <47362640+Courgax@users.noreply.github.com>
2019-04-11 20:56:04 +0200

    Ajout d'image

--- WebPortal/application/views/home.php
+++ WebPortal/application/views/home.php
@@ -9,7 +9,7 @@
 <section>		
 	<article>
 		<h3>
-			<img src="tofill.png" alt="IMAGE " />Actualit&eacute;s
+			<img  src="<?php echo base_url(); ?>assets/images/vache.jpg" alt="IMAGE " />Actualit&eacute;s
 		</h3>
 		<p>
 			Cumque pertinacius ut legum gnarus accusatorem flagitaret atque sollemnia, doctus id Caesar libertatemque superbiam ratus tamquam obtrectatorem audacem excarnificari praecepit, qui ita evisceratus ut cruciatibus membra deessent, inplorans caelo iustitiam, torvum renidens fundato pectore mansit inmobilis nec se incusare nec quemquam alium passus et tandem nec confessus nec confutatus cum abiecto consorte poenali est morte multatus. et ducebatur intrepidus temporum iniquitati insultans, imitatus Zenonem illum veterem Stoicum qui ut mentiretur quaedam laceratus diutius, avulsam sedibus linguam suam cum cruento sputamine in oculos interrogantis Cyprii regis inpegit.
