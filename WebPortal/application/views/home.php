99c0411616f987f9b6344b2488350167746e6e36
Courgax <47362640+Courgax@users.noreply.github.com>
2019-04-20 09:52:25 +0200

    Tache 1

--- WebPortal/application/views/home.php
+++ WebPortal/application/views/home.php
@@ -6,10 +6,11 @@
 		<h3>Nombre de visites par jour</h3>
 	</aside>
 </section>
-<section>		
-	<article>
+<section>	
+	<h2>Actualit&eacute;s</h2>	
+	<article>		
 		<h3>
-			<img  src="<?php echo base_url(); ?>assets/images/vache.jpg" alt="IMAGE " />Actualit&eacute;s
+		<img  src="<?php echo base_url(); ?>assets/images/vache.jpg" class="TextWrap" alt="IMAGE " />
 		</h3>
 		<p>
 			Cumque pertinacius ut legum gnarus accusatorem flagitaret atque sollemnia, doctus id Caesar libertatemque superbiam ratus tamquam obtrectatorem audacem excarnificari praecepit, qui ita evisceratus ut cruciatibus membra deessent, inplorans caelo iustitiam, torvum renidens fundato pectore mansit inmobilis nec se incusare nec quemquam alium passus et tandem nec confessus nec confutatus cum abiecto consorte poenali est morte multatus. et ducebatur intrepidus temporum iniquitati insultans, imitatus Zenonem illum veterem Stoicum qui ut mentiretur quaedam laceratus diutius, avulsam sedibus linguam suam cum cruento sputamine in oculos interrogantis Cyprii regis inpegit.
