15e16dd82421d1e3949dccd2f0178af2b655b9a3
Courgax <47362640+Courgax@users.noreply.github.com>
2019-04-20 11:15:35 +0200

    Test bancale sur l'affichage du nombre d'agri

--- WebPortal/application/controllers/Home.php
+++ WebPortal/application/controllers/Home.php
@@ -2,9 +2,16 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
+    public function __construct(){
+        // TODO ensure that user is logged
+        parent::__construct();
+        $this->load->model('UserModel');
+    }    
+
     public function index(){
         $this->load->view('header');
-        $this->load->view('home');
+        $data['numberAgri'] = $this->UserModel->getNumberFromRole(4);
+        $this->load->view('home',$data);
         $this->load->view('footer');
     }
 }
--- WebPortal/application/views/home.php
+++ WebPortal/application/views/home.php
@@ -4,6 +4,7 @@
 	<aside>
 		<h3>Nombre d'agriculteurs inscrits</h3>
 		<h3>Nombre de visites par jour</h3>
+		<?php echo form_open("home/index/$numberAgri");?>
 	</aside>
 </section>
 <section>	
