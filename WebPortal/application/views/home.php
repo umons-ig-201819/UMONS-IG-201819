68c523796caa54f060cabce41b2b508afcddd050
Courgax <47362640+Courgax@users.noreply.github.com>
2019-04-20 13:35:06 +0200

    2nd home image

--- WebPortal/application/views/home.php
+++ WebPortal/application/views/home.php
@@ -20,9 +20,10 @@
 	</article>	
 </section>
 <section>
+	<h2>Info du de la semaine</h2>	
 	<article>
 		<h3>
-		<img src="tofill.png" alt="IMAGE " />G&eacute;n&eacute;ralit&eacute;s
+		<img  src="<?php echo base_url(); ?>assets/images/crops.jpg" class="TextWrap" alt="IMAGE " />
 		</h3>
 		<p>
 			Cumque pertinacius ut legum gnarus accusatorem flagitaret atque sollemnia, doctus id Caesar libertatemque superbiam ratus tamquam obtrectatorem audacem excarnificari praecepit, qui ita evisceratus ut cruciatibus membra deessent, inplorans caelo iustitiam, torvum renidens fundato pectore mansit inmobilis nec se incusare nec quemquam alium passus et tandem nec confessus nec confutatus cum abiecto consorte poenali est morte multatus. et ducebatur intrepidus temporum iniquitati insultans, imitatus Zenonem illum veterem Stoicum qui ut mentiretur quaedam laceratus diutius, avulsam sedibus linguam suam cum cruento sputamine in oculos interrogantis Cyprii regis inpegit.
