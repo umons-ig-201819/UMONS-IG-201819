628f802223d246f8352af08db64df5ed32d385c6
Jean-Sébastien Lerat <belegkarnil@10.0.0.125>
2019-04-24 21:52:04 +0200

    t

--- WebPortal/application/views/mysources.php
+++ WebPortal/application/views/mysources.php
@@ -3,7 +3,7 @@
 		<h1>Gestion de mes sources de données</h1>
 		<ul>
 		<?php foreach($data as $source): ?>
-		<li><?="$data[id] $data[filename] $data[url] $data[visible]";?></li>
+		<li><?="$data[id] $data[file_name] $data[url] $data[visible]";?></li>
 		<?php endforeach; ?>
 		</ul>
 	</article>
