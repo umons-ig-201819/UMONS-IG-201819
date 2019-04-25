22f580c6a3904c91271fd1653865c34470c97dd9
Jean-Sébastien Lerat <belegkarnil@localhost.belegkarnil-shs>
2019-04-25 06:15:46 +0200

    add link to manage sources

--- WebPortal/application/views/datasource.php
+++ WebPortal/application/views/datasource.php
@@ -13,6 +13,7 @@ echo form_fieldset_close();
 echo form_close();
 		?>
 		<p><a href="<?=site_url("datasource/addSource"); ?>">Ajouter un fichier de donn&eacute;es</a></p>
+		<p><a href="<?=site_url("datasource/manage"); ?>">Gérer mes sources de donn&eacute;es et acc&agrave;s</a></p>
 	</article>
 	<?php foreach($url as $zeppelin_link): ?>
 	<!--  sandbox="allow-plugins allow-scripts allow-same-origin"  --> 
