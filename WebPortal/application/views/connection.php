eea17ed5d104ebdbe0e4740fb54fead2614da24c
Microm <Microm@192.168.3.7>
2019-04-25 22:42:57 +0200

    test css

--- WebPortal/application/views/connection.php
+++ WebPortal/application/views/connection.php
@@ -13,16 +13,17 @@ else:
         echo form_open('connection');
       
         echo form_fieldset('Informations de connexion&nbsp;:');
-        echo '<p>'; ?>
+        echo '<p>'; 
+        ?>
         	<li><?php echo form_label('Identifiant','username'); ?></li> 
             <li><?php echo form_input('username',$this->input->post('username',TRUE),'id="username" required="required"'); ?></li>
         <?php 
         echo '</p>';
         echo '<p>';
-        ?><article class="connexion"><?php 
-                echo form_label('Mot de passe','password');
-                echo form_password('password','','id="password" required="required"');
-            ?></article><?php 
+        ?>
+            <li><?php echo form_label('Mot de passe','password'); ?></li> 
+            <li><?php echo form_password('password','','id="password" required="required"'); ?></li> 
+        <?php 
         echo '</p>';
         echo form_submit('action', 'Se connecter');
         echo form_fieldset_close();
