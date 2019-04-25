11353fb98584376104d76bd50db2965edfb965da
Microm <Microm@192.168.3.7>
2019-04-25 23:46:08 +0200

    suppression cadre rouge

--- WebPortal/application/views/register.php
+++ WebPortal/application/views/register.php
@@ -10,17 +10,17 @@
 <fieldset>
 <legend> Informations d'inscription :  </legend>
 	 	<label >Nom d'utilisateur (Login) : </label>
-	 	<input type="text" name="login" style="border-color:darkblue" value="<?php echo set_value('login');?>">
+	 	<input type="text" name="login" value="<?php echo set_value('login');?>">
 	 	<font color = 'red'>*</font><br><br>
    		<label >Sexe : </label>
     		<input type="radio" name="gender" value="1" <?php echo set_radio('gender',1,TRUE);?>> Homme&nbsp;&nbsp;
 			<input type="radio" name="gender" value="0" <?php echo set_radio('gender',0);?>> Femme&nbsp;&nbsp;
 			<input type="radio" name="gender" value="2" <?php echo set_radio('gender',2);?>> Autre<br><br>
     	<label >Nom : </label>
-    	<input type="text" name="lastname" style="border-color:red" value="<?php echo set_value('lastname');?>">
+    	<input type="text" name="lastname" value="<?php echo set_value('lastname');?>">
     	<font color = 'red'>*</font><br><br>
     	<label >Pr&eacute;nom : </label>
-    	<input type="text" name="firstname" style="border-color:red" value="<?php echo set_value('firstname');?>">
+    	<input type="text" name="firstname" value="<?php echo set_value('firstname');?>">
     	<font color = 'red'>*</font><br><br>
     	<label >Date de naissance : </label>
     	<input type="date" name="birthdate" value="<?php echo set_value('birthdate');?>"><br><br>
@@ -31,9 +31,9 @@
     	<label >Num&eacute;ro de GSM : </label>
     	<input type="text" name="mobile" value="<?php echo set_value('mobile');?>"><br><br>
  		<label >Mot de passe : </label>
- 		<input type="password" id="mdp" name="password" style="border-color:red" placeholder="Votre mot de passe" value="<?php echo set_value('password');?>"/>
+ 		<input type="password" id="mdp" name="password" placeholder="Votre mot de passe" value="<?php echo set_value('password');?>"/>
   	 		<font color = 'red'>*</font>
-			<input type="password" id="confirm_mdp" name="confirm_mdp" style="border-color:red" placeholder="Confirmation du mot de passe" value="<?php echo set_value('confirm_mdp');?>"/>
+			<input type="password" id="confirm_mdp" name="confirm_mdp" placeholder="Confirmation du mot de passe" value="<?php echo set_value('confirm_mdp');?>"/>
 			<font color = 'red'>*</font><br><br>
     		<input type="submit" value="Cr&eacute;er le compte" name="registering"><br><br>
     		<font color = 'darkblue'>* champs obligatoire</font>
