ea29170357be2e3a73eb404641529e60804a6426
Microm <Microm@192.168.3.7>
2019-04-30 20:32:50 +0200

    help

--- WebPortal/application/views/help.php
+++ WebPortal/application/views/help.php
@@ -1,15 +1,15 @@
-<h1>ABOUT</h1>
+<h1>A propos</h1>
 <section>
-Ce site a &eacute;t&eacute; d&eacute;velopp&eacute; &agrave; l\'aide du frameWork Codeigniter par : <br>
+<br><br>Ce site a &eacute;t&eacute; d&eacute;velopp&eacute; &agrave; l\'aide du frameWork Codeigniter par : <br><br>
 
 COOLS Aur&eacute;lie<br>
 GAUTHIER In&egrave;s<br>
 GENIN Emilie<br>
 VANDEN DRIES Virginie<br>
-LERAT Jean\-S&eacute;bastien<br>
+LERAT Jean-S&eacute;bastien<br>
 ROBBERTS Fran&ccedil;ois<br>
 ROMBAUX Micha&euml;l<br><br>
 
-<a href='https://github.com/umons-ig-201819' >Visiter notre Github</a>
+<a href='https://github.com/umons-ig-201819' >Visitez notre Github</a><br><br>
 
 </section>
