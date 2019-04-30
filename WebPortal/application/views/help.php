e222a40d6e5ecbd04e4d85b16501215255efe3ff
@Rombaux <Rombaux@192.168.3.7>
2019-04-30 19:07:17 +0200

    about

--- WebPortal/application/views/help.php
+++ WebPortal/application/views/help.php
@@ -1,10 +1,7 @@
 <h1>Connexion</h1>
 <section>
 		<?php        
-            echo form_open('connection');
-            echo form_fieldset('Informations:');
-            echo form_submit('action', 'Se connecter');
-            echo form_fieldset_close();
-            echo form_close();
+            echo ('about');
+            echo ('Informations:');
         ?>
 </section>
