de49fc510c258ee3f1bfbf5d9847b5b6a7abeb24
Jean-Sébastien Lerat <belegkarnil@10.0.0.125>
2019-04-30 11:47:12 +0200

    rights

--- WebPortal/application/views/header.php
+++ WebPortal/application/views/header.php
@@ -27,6 +27,7 @@
                             <li><a href="<?php echo site_url("help"); ?>">&Agrave; propos</a></li>
                             <?php if(isset($this->session->UserID)): ?>
                             <li><a href="<?php echo site_url("connection/logout"); ?>">D&eacute;connexion</a></li>
+                            <?php print_r($this->session); ?>
                             <?php if(array_key_exists('MANAGE_PROJECT', $this->session->Rights)): ?>
                             <li><a href="<?php echo site_url("administration"); ?>">Gestion</a></li>
                             <?php endif; ?>
