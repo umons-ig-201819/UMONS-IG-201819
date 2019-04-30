451327c9bc2e465879b0b143b8dba6c02ece86d7
Microm <Microm@192.168.3.7>
2019-04-30 18:38:21 +0200

    about

--- WebPortal/application/views/help.php
+++ WebPortal/application/views/help.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html>
-    <head>
-    
-    <meta charset="utf-8" />
-    <link rel="stylesheet" type="text/css"
-    	href="<?php echo base_url(); ?>assets/css/style.css">
-<script type="text/javascript" src="<?php echo base_url(); ?>assets/javascript/home.js"></script>
-    <title>Portail AWE</title>
-    </head>
-    
-    <body>    
-    <?php include ("header.php")?>
-    
-    <h1>Help page</h1>
-    
-    <?php include ("footer.php")?>
-    </body>
-</html>
+<h1>Connexion</h1>
+<section>
+		<?php        
+            echo form_open('connection');
+            echo form_fieldset('Informations:');
+            echo form_submit('action', 'Se connecter');
+            echo form_fieldset_close();
+            echo form_close();
+        ?>
+</section>
