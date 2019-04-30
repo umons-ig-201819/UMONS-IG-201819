7dc408dfe6fa3d9c8e2dc817ed89908a60324638
Microm <Microm@192.168.3.7>
2019-04-30 20:16:16 +0200

    about

--- WebPortal/application/views/help.php
+++ WebPortal/application/views/help.php
@@ -1,1 +1,15 @@
 		<h1>ABOUT</h1>
+<section>
+Ce site a été développé à l'aide du frameWork Codeigniter par : 
+
+COOLS Aurélie
+GAUTHIER Inès
+GENIN Emilie
+VANDEN DRIES Virginie
+LERAT Jean-Sébastion
+ROBBERTS François
+ROMBAUX Michaël
+
+<?php "<a href='https://github.com/umons-ig-201819' >Visiter notre Github</a>";?>
+
+</section>
