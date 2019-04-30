2ba5e0e6ca08616c96fff22373c12632a578a452
Microm <Microm@192.168.3.7>
2019-04-30 19:55:25 +0200

    about

--- WebPortal/application/views/help.php
+++ WebPortal/application/views/help.php
@@ -1,7 +1,34 @@
-<h1>Connexion</h1>
-<section>
-		<?php        
-            echo ('about');
-            echo ('Informations:');
+		<h1>ABOUT</h1>
+<?php
+if(isset($this->session->UserID)):
+?>		<p id="success">Bienvenue <?=ucfirst($firstname).' '.strtoupper($lastname);?>.</p>
+
+<?php
+else:
+        if(isset($error)):
+?>
+		<p id="error"><font color="red">Erreur de login/mot de passe.</font></p>
+<?php endif;
+        ?><section><?php        
+        echo form_open('connection');
+        echo form_fieldset('Informations de connexion&nbsp;:');
+        echo '<p>'; 
         ?>
+        	<label><?php echo form_label('Identifiant : ','username'); ?></label> 
+            <?php 
+            echo form_input('username',$this->input->post('username',TRUE),'id="username" required="required"');  
+            echo '<br>';
+            ?>
+            <label><?php echo form_label('Mot de passe : ','password'); ?></label> 
+            <?php
+            echo form_password('password','','id="password" required="required"');
+            echo '<br>';
+            ?>
+        <?php 
+        echo '</p>';
+        echo form_submit('action', 'Se connecter');
+        echo form_fieldset_close();
+        echo form_close();
+endif;
+?>
 </section>
