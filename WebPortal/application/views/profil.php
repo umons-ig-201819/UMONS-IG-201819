2fe2ce1a93ec2631315a30e0941f38eb0e288fff
Microm <Microm@192.168.3.7>
2019-05-03 23:11:45 +0200

    test delete

--- WebPortal/application/views/profil.php
+++ WebPortal/application/views/profil.php
@@ -1,6 +1,6 @@
 <script>
     var url="<?php echo base_url();?>";
-    function delete(id){
+    function supprofil(id){
        var r=confirm("Do you want to delete this?")
         if (r==true)
           window.location = url+"profil/remove/"+id;
@@ -115,7 +115,7 @@ echo form_close();
 	<?php
 	echo validation_errors();
 	?>
-	<a href="javascript:void(0);" onclick="delete(<?php echo $user_id;?>);">Supprimer mon compte'</a>
+	<a href="javascript:void(0);" onclick="supprofil(<?php echo $user_id;?>);">Supprimer mon compte</a>
 <!--     	echo form_open("profil/remove/$user_id");
 	echo form_submit('action', 'Supprimer mon compte',"class='button' onclick='delete($user_id)'");
 	echo form_close();
