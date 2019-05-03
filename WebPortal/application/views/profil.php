d84c621e5e0061a4d20313411d977b788a8fc74c
Microm <Microm@192.168.3.7>
2019-05-03 23:23:30 +0200

    test delete

--- WebPortal/application/views/profil.php
+++ WebPortal/application/views/profil.php
@@ -113,12 +113,12 @@ echo form_close();
 </section>
 	<article id="centerButton">
 	<?php
-	echo validation_errors();
-	?>
-	<a href="javascript:void(0);" onclick="supprofil(<?php echo $user_id;?>);">Supprimer mon compte</a>
-<!--     	echo form_open("profil/remove/$user_id");
-	echo form_submit('action', 'Supprimer mon compte',"class='button' onclick='delete($user_id)'");
+	echo validation_errors();/*
+    ?>
+<!--	<a href="javascript:void(0);" onclick="supprofil(<?php // echo $user_id;?>);">Supprimer mon compte</a>-->*/
+    echo form_open("profil/remove/$user_id");
+	echo form_submit('action', 'Supprimer mon compte',"class='button' onclick='supprofil($user_id)'");
 	echo form_close();
-	?>	-->
+	?>	
 	</article>
 
