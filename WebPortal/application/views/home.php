deff9d3fd8dafc016047a43e8b32116a2af2eab6
Courgax <47362640+Courgax@users.noreply.github.com>
2019-05-03 17:42:03 +0200

    Test image

--- WebPortal/application/views/home.php
+++ WebPortal/application/views/home.php
@@ -30,9 +30,9 @@ echo '<strong>'.$compte.'</strong> visites.';
 <section>	
 	<h2>Actualit&eacute;s</h2>	
 	<article>		
-		<h3>
+		
 		<img  src="<?php echo base_url(); ?>assets/images/vache.jpg" class="TextWrap" alt="IMAGE " />
-		</h3>
+		
 		<p>
 			Cumque pertinacius ut legum gnarus accusatorem flagitaret atque sollemnia, doctus id Caesar libertatemque superbiam ratus tamquam obtrectatorem audacem excarnificari praecepit, qui ita evisceratus ut cruciatibus membra deessent, inplorans caelo iustitiam, torvum renidens fundato pectore mansit inmobilis nec se incusare nec quemquam alium passus et tandem nec confessus nec confutatus cum abiecto consorte poenali est morte multatus. et ducebatur intrepidus temporum iniquitati insultans, imitatus Zenonem illum veterem Stoicum qui ut mentiretur quaedam laceratus diutius, avulsam sedibus linguam suam cum cruento sputamine in oculos interrogantis Cyprii regis inpegit.
 			Cumque pertinacius ut legum gnarus accusatorem flagitaret atque sollemnia, doctus id Caesar libertatemque superbiam ratus tamquam obtrectatorem audacem excarnificari praecepit, qui ita evisceratus ut cruciatibus membra deessent, inplorans caelo iustitiam, torvum renidens fundato pectore mansit inmobilis nec se incusare nec quemquam alium passus et tandem nec confessus nec confutatus cum abiecto consorte poenali est morte multatus. et ducebatur intrepidus temporum iniquitati insultans, imitatus Zenonem illum veterem Stoicum qui ut mentiretur quaedam laceratus diutius, avulsam sedibus linguam suam cum cruento sputamine in oculos interrogantis Cyprii regis inpegit.
