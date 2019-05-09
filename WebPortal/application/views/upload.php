331b416ce6e3ca855d94b856b30e4f3707524ac3
ACOOLS <47692726+ACOOLS@users.noreply.github.com>
2019-05-09 20:44:34 +0200

    Update upload.php

--- WebPortal/application/views/upload.php
+++ WebPortal/application/views/upload.php
@@ -1,5 +1,8 @@
+<section id="top_page">
+<h1>Mes fichiers et applications</h1>
+</section>
+
 <section>
-	<h1>Mes fichiers et applications</h1>
 	<?php if(! is_null($error)): ?>
 		<?php if(array_key_exists('error', $error)): ?>
 	<article class="error"><p><?=$error['error'];?></p></article>
@@ -12,8 +15,10 @@
 		<?php
 		echo form_open_multipart("datasource/addSource");
 		echo form_fieldset('Envoyer un fichier de données');
+		?> <br></br>
+		<?php
 		echo '<input type="file" id="datafile" name="datafile" multiple="false" required="required" accept=".csv,.accdb,.mdb,application/msaccess,application/x-msaccess,text/csv">';
-		echo form_submit('action', 'Envoyer');
+		echo form_submit('action', 'Envoyer',"class='buttonvalider'");
 		echo form_fieldset_close();
 		echo form_close();
 		?>
