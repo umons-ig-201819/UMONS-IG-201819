fb463aafa65d63f4d4c672378e572dbf9e747029
ACOOLS <47692726+ACOOLS@users.noreply.github.com>
2019-05-09 20:42:23 +0200

    Update profil.php

--- WebPortal/application/views/profil.php
+++ WebPortal/application/views/profil.php
@@ -9,8 +9,10 @@
     }
 </script>
 
-<section>
+<section id="top_page">
 <h1>Mon profil</h1>
+</section
+<section>
 	<article id="account_information">
 		<?php
 echo form_open("profil/update/$user_id");
@@ -35,9 +37,13 @@ echo '</p>';
 echo '<p>';
     echo form_label('Sexe : ');
     echo '<br>';
-    echo form_label('Homme','gender_enabled').form_radio('gender','1',$gender == 1, 'id="gender_enabled"');
-    echo form_label('Femme','gender_enabled').form_radio('gender','0',$gender == 0, 'id="gender_enabled"');
-    echo form_label('Autre','gender_enabled').form_radio('gender','2',$gender == 2, 'id="gender_enabled"');
+    $attributes = array(
+        'style'=>'max-width: 30%;min-width: 7%; margin-left:7%;'
+ // or inline css, this is black color
+    );
+    echo form_label('Homme','gender_enabled',$attributes).form_radio('gender','1',$gender == 1, 'id="gender_enabled"');
+    echo form_label('Femme','gender_enabled',$attributes).form_radio('gender','0',$gender == 0, 'id="gender_enabled"');
+    echo form_label('Autre','gender_enabled',$attributes).form_radio('gender','2',$gender == 2, 'id="gender_enabled"');
     //    echo form_input('gender',$gender,'id="gender"');/*** TODO input type to radio (HTML5) **/
 echo '</p>';
 echo '<p>';
@@ -52,7 +58,9 @@ echo '<p>';
     echo form_label('G.S.M. : ','mobile');
     echo '<input type="mobile" name="mobile" value="'.$mobile.'" id="mobile">';
 echo '</p>';
-echo form_submit('action', 'Modifier');
+    $attributes2 = array(
+        'style'=>'background-color:#cff3b7;float:right');
+    echo form_submit('action', 'Modifier',"class='buttonvalider'");
 echo form_fieldset_close();
 echo form_close();
 
@@ -65,10 +73,12 @@ echo '</p>';*/
 echo '<p>';
     echo form_label('Nouveau mot de passe : ','motdepasse');
     echo '<input type="password" name="password" >';
-    echo form_label('Confirmation : ','motdepasseconfirm');
+    $attr = array(
+        'style'=>'margin-left:35px;');
+    echo form_label('Confirmation : ','motdepasseconfirm',$attr);
     echo '<input type="password" name="passwordconfirm" >';
 echo '</p>';
-echo form_submit('action', 'Modifier');
+echo form_submit('action', 'Modifier',"class='buttonvalider'");
 echo form_fieldset_close();
 echo form_close();
 		?>	
@@ -81,21 +91,21 @@ echo form_open("profil/rights/$user_id");
 echo form_fieldset('Droits d\'acc&egrave;s');
 echo '<p>';
     echo form_label('Partager mes sources de donn&eacute;es : ');
-    echo form_label('Autoriser','sharing_allow').form_radio('sharing','2',$sharing == 2, 'id="sharing_allow"');
-    echo form_label('Demande d\'accord','sharing_request').form_radio('sharing','1',$sharing == 1, 'id="sharing_request"');
-    echo form_label('Refuser','sharing_refuse').form_radio('sharing','0',$sharing == 0, 'id="sharing_refuse"');
+    echo form_label('Autoriser','sharing_allow',$attributes).form_radio('sharing','2',$sharing == 2, 'id="sharing_allow"');
+    echo form_label('Demande d\'accord','sharing_request',$attributes).form_radio('sharing','1',$sharing == 1, 'id="sharing_request"');
+    echo form_label('Refuser','sharing_refuse',$attributes).form_radio('sharing','0',$sharing == 0, 'id="sharing_refuse"');
 echo '</p>';
 echo '<p>';
     echo form_label('Recevoir les requ&ecirc;tes d\'acc&egrave;s aux donn&eacute;es (si le partage n\'est pas d\'office autoris&eacute;) : ');
-    echo form_label('Accepter','advise_enabled').form_radio('advice','1',$advice == 1, 'id="advice"');
-    echo form_label('Refuser','advise_disabled').form_radio('advice','0',$advice == 0, 'id="advice"');
+    echo form_label('Accepter','advise_enabled',$attributes).form_radio('advice','1',$advice == 1, 'id="advice"');
+    echo form_label('Refuser','advise_disabled',$attributes).form_radio('advice','0',$advice == 0, 'id="advice"');
 echo '</p>';
 //echo '<p>';
 //   echo ('En connaissance du R.G.P.D., j\'accepte que Wallesmart utilise mes données &agrave; des fins statistiques.');
 //   echo form_radio('gdpr','1',$gdpr, 'id="gdpr_enabled"').form_label('Accepter','gdpr_enabled');
 //  echo form_radio('gdpr','0',!$gdpr, 'id="gdpr_disabled"').form_label('Refuser','gdpr_disabled');
 //echo '</p>';
-echo form_submit('action', 'Modifier');
+echo form_submit('action', 'Modifier', "class='buttonvalider'" );
 echo form_fieldset_close();
 echo form_close();
 
@@ -132,8 +142,8 @@ echo form_close();
 	echo validation_errors();
 
     echo form_open("profil/remove/$user_id");
-	echo form_submit('action', 'Supprimer mon compte',"class='button' onClick='return supprofil();'");
-	echo form_close();
+	echo form_submit('action', 'Supprimer mon compte',"class='buttonsupprimer' onClick='return supprofil();'");
+	echo form_close(); 
 	?>	
 	</article>
 
