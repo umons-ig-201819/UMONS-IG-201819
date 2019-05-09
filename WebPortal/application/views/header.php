3514fc698a1480e02b2c9f4029898baa37098f47
ACOOLS <47692726+ACOOLS@users.noreply.github.com>
2019-05-09 20:40:22 +0200

    Update header.php

--- WebPortal/application/views/header.php
+++ WebPortal/application/views/header.php
@@ -1,48 +1,90 @@
 <!DOCTYPE html>
 <html>
     <head>
+    	<meta name="viewport" content="width=device-width, initial-scale=1">
     	<meta charset="UTF-8">
     	<title>Wallesmart</title>
     	<!--[if lt IE 9]>
     		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     	<![endif]-->
     	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script> <!--  Ajout de la largeur de l'�cran pour le d�v-->
-    	<link rel="stylesheet" type="text/css" media="only screen" href="<?php echo base_url(); ?>assets/css/style.css" />
+    	<link rel="stylesheet" type="text/css"  href="<?php echo base_url(); ?>assets/css/style.css" />
+    	<link rel="stylesheet" type="text/css"  href="<?php echo base_url(); ?>assets/css/font-awesome.min.css" />
+    	<link rel="icon" href="assets/images/Wallesmart.ico" />
     	<style type="text/css">
         	.alert-warning {
         	   visibility: hidden;
         	   display: none;
         	}
     	</style>
+    
+        
     </head>
     <body>
-    	<header>         
-                <div id="logo">
-                	<h1> Wallesmart </h1>
-            		<nav>    		
-            			<ul>
-            				<li><a href="<?php echo site_url("home"); ?>">Accueil</a></li>
-                            <li><a href="<?php echo site_url("help"); ?>">&Agrave; propos</a></li>
-                            <?php if(isset($this->session->UserID)): ?>
-                            <li><a href="<?php echo site_url("connection/logout"); ?>">D&eacute;connexion</a></li>
-                            <?php if(in_array('MANAGE_PROJECT', $this->session->Rights)): ?>
-                            <li><a href="<?php echo site_url("administration"); ?>">Gestion</a></li>
-                            <?php endif; ?>
-                            <li><a href="<?php echo site_url("profil"); ?>">Mon profil</a></li>
-                            <li><a href="<?php echo site_url("datasource"); ?>">Mes donn&eacute;es</a></li>
-                            <li><a href="<?php echo site_url("search/user"); ?>">Rechercher un utilisateur</a></li>
-                            <li><a href="<?php echo site_url("search/datasource"); ?>">Rechercher des donn&eacute;es</a></li>
-                            <?php else: ?>
-                            <li><a href="<?php echo site_url("connection"); ?>">Connexion</a></li>
-                            <li><a href="<?php echo site_url("register"); ?>">Inscription</a></li>
-                            <?php endif; ?>
-                      <!--     <li><a href="?php echo site_url("connection/register"); ?>">Inscription</a></li>        -->              
-            			</ul>
-            		</nav>
-    			</div>
+       
+              <div id="logo">
+                	<!--  <img src="<?php echo base_url(); ?>assets/images/Wallesmart2.png" alt="Logo Principal AWE!" />-->
+                	
+                	
+                </div>
+    
+    	<div class="header" id="myHeader">
     	
-    			
-    <!--  Ajout de la largeur de l'�cran pour le d�v-->
-    <div style="background:pink;color:#333;position:fixed;right:0;bottom:0;z-index:99999999;font:1em arial;opacity:.9" id="ld"></div><script>setInterval(function(){if($(window).height()>=$(document).height()){$('#ld').text($(document).width()+' px');}else{$('#ld').text($(document).width()+17+' px');}},150);
+    <!--  <div class="topnav" id="myTopnav">-->	
+            		   	
+            				<a href="<?php echo site_url("home"); ?>">Accueil</a>
+                            <a href="<?php echo site_url("help"); ?>">&Agrave; propos</a>
+                            <?php if(isset($this->session->UserID)): ?>
+                            
+                            <?php if(in_array('MANAGE_PROJECT', $this->session->Rights)): ?>
+                            <a href="<?php echo site_url("administration"); ?>">Gestion</a>
+                            <?php endif; ?>
+                            <a href="<?php echo site_url("profil"); ?>">Mon profil</a>
+                            <a href="<?php echo site_url("datasource"); ?>">Mes donn&eacute;es</a>
+                            <a href="<?php echo site_url("search/user"); ?>">Rechercher un utilisateur</a>
+                            <a href="<?php echo site_url("search/datasource"); ?>">Rechercher des donn&eacute;es</a>
+                            <?php else: ?>
+                            <a href="<?php echo site_url("connection"); ?>">Connexion</a>
+                            <a href="<?php echo site_url("register"); ?>">Inscription</a>
+                            
+                            <?php endif; ?>
+                            <?php if(isset($this->session->UserID)): ?>
+                            <a href="<?php echo site_url("connection/logout"); ?>">D&eacute;connexion</a>
+                            <?php endif; ?>
+                            <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+                      <!--     <li><a href="?php echo site_url("connection/register"); ?>">Inscription</a></li>        -->              
+            			
+        
+    			</div>
+  </div>
+
+
+<script type="text/javascript">
+function myFunction() {
+    var x = document.getElementById("myHeader");
+    if (x.className === "header") {
+      x.className += " responsive";
+    } else {
+      x.className = "header";
+    }
+  }
+
+
+
+    	window.onscroll = function() {myFunction2()};
+
+    	var header = document.getElementById("myHeader");
+    	var sticky = header.offsetTop;
+
+    	function myFunction2() {
+    	  if (window.pageYOffset > sticky) {
+    	    header.classList.add("sticky");
+    	  } else {
+    	    header.classList.remove("sticky");
+    	  }
+    	}
+
+       
+
 </script>
-    	</header>
+	
