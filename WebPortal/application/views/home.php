2726b4a5529e5fa4ed569d3c6c8dae8fee1a0177
ACOOLS <47692726+ACOOLS@users.noreply.github.com>
2019-05-10 20:34:52 +0200

    div pour css compteur

--- WebPortal/application/views/home.php
+++ WebPortal/application/views/home.php
@@ -10,12 +10,12 @@
     </div>
 
 	<div class="col-3">
-		<aside id="visits" style="margin-top: 10px";>
-		<h3>Nombre d'agriculteurs inscrits : </h3><br> 
-			<?=implode("\n",array_map(function($x) { return "<span>$x</span>"; }, str_split(sprintf("%05d",intval($numberAgri))))); ?>
-
-    	<h3  id="visits" >Nombre de visites : </h3><br>
-    		<?=implode("\n",array_map(function($x) { return "<span>$x</span>"; }, str_split(sprintf("%05d",intval($numberVisit))))); ?>
+			<aside id="visits" style="margin-top: 10px";>
+		<h3>Nombre d'agriculteurs inscrits : </h3>
+		<div id="compteur"><?=implode("\n",array_map(function($x) { return "<span>$x</span>"; }, str_split(sprintf("%05d",intval($numberAgri))))); ?></div>
+	<br> 
+    	<h3  id="visits" >Nombre de visites : </h3>
+    		<div id="compteur"><?=implode("\n",array_map(function($x) { return "<span>$x</span>"; }, str_split(sprintf("%05d",intval($numberVisit))))); ?>
 	</div>
     	</aside> 
 	</div>
