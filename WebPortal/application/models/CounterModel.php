6a5953cf38de356a39b5f22bd9fb39f4b136279b
Jean-Sébastien Lerat <belegkarnil@localhost.belegkarnil-shs>
2019-05-10 20:28:20 +0200

    implement CounterModel

--- WebPortal/application/models/CounterModel.php
+++ WebPortal/application/models/CounterModel.php
@@ -5,10 +5,15 @@ class CounterModel extends CI_Model{
         $this->load->database();
     }
     public function newVisitor(){
-        
+        $sql    = "INSERT IF NOT EXISTS counter (day) VALUES (CURRENT_DATE())";
+        $this->db->query($sql);
+        $sql    = "UPDATE counter SET value=value+1 WHERE day=CURRENT_DATE()";
+        $this->db->query($sql);
     }
     
     public function countVisitors(){
-        return 0;
+        $sql    = "SELECT MAX(0,value) AS value FROM counter WHERE day=CURRENT_DATE()";
+        $query  = $this->db->query($sql)->row_array();
+        return $query['value'];
     }
 }
