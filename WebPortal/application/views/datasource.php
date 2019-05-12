b413382993c26ab461e18630b9d12641a33b33c6
EmiGenin <em.genin@gmail.com>
2019-05-12 21:41:19 +0200

    correction css bouton

--- WebPortal/application/views/datasource.php
+++ WebPortal/application/views/datasource.php
@@ -1,13 +1,15 @@
+	<section id="top_page">
+		<h1>Sources de donn&eacute;es</h1>
+	</section>
 <section>
 	<article>
-		<h1>Sources de donn&eacute;es</h1>
 <?php
 echo form_open('datasource');
 echo form_fieldset('Sources de donn&eacute;es');
 echo '<p>';
     echo form_label('Source');
     echo form_dropdown('datasource',$options,$selected,'id="datasource" required="required"');
-    echo form_submit('action', 'Charger');
+    echo form_submit('action', 'Charger',"class='buttonvalider'");
 echo '</p>';
 echo form_fieldset_close();
 echo form_close();
