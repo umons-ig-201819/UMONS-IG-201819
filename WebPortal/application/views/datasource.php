ac7b1ad09868f7ca7325a9f2b77fe20b60404830
ACOOLS <47692726+ACOOLS@users.noreply.github.com>
2019-05-13 07:15:13 +0200

    Bouton - lien

--- WebPortal/application/views/datasource.php
+++ WebPortal/application/views/datasource.php
@@ -9,13 +9,14 @@ echo form_fieldset('Sources de donn&eacute;es');
 echo '<p>';
     echo form_label('Source');
     echo form_dropdown('datasource',$options,$selected,'id="datasource" required="required"');
+    ?> <br><br> <?php 
     echo form_submit('action', 'Charger',"class='buttonvalider'");
 echo '</p>';
 echo form_fieldset_close();
 echo form_close();
 		?>
-		<p><a href="<?=site_url("datasource/addSource"); ?>">Ajouter un fichier de donn&eacute;es</a></p>
-		<p><a href="<?=site_url("datasource/manage"); ?>">G&eacute;rer mes sources de donn&eacute;es et acc&egrave;s</a></p>
+		<input type="button" value="Ajouter un fichier de donn&eacute;es" id="lien" onclick="window.location.href='<?=site_url("datasource/addSource"); ?>'"> </input>
+		<input type="button" value="Gérer mes sources de donn&eacute;es et acc&egrave;s" id="lien" onclick="window.location.href='<?=site_url("datasource/manage"); ?>'"></input> 
 	</article>
 	<?php foreach($url as $zeppelin_link): ?>
 	<!--  sandbox="allow-plugins allow-scripts allow-same-origin"  --> 
