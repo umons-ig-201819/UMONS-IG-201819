16256000296fdbe35953ea16c1c359a8fdf05aa2
Jean-Sébastien Lerat <belegkarnil@169.254.12.4>
2019-05-14 09:37:07 +0200

    (in-dev) API, list function

--- WebPortal/application/controllers/Rest.php
+++ WebPortal/application/controllers/Rest.php
@@ -42,7 +42,7 @@ class Rest extends CI_Controller {
         $methods = get_class_methods('Rest');
         $methods = array_map(function ($x){ return new ReflectionMethod($this, $x);}, $methods);
         $methods = array_filter($methods,function ($x){ return $x->isPublic() && !$x->isConstructor();});
-        print_r(array_map(function ($x){ $x->getDeclaringClass()->getName();}, $methods));
+        print_r(array_map(function ($x){ $x->getDeclaringClass();}, $methods));
         $methods = array_map(function ($x){ return $x->getName();}, $methods);
         $this->response['body']['methods'] = $methods;
         $this->output();
