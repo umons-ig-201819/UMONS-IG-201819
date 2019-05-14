2d1f0348b94da471ef51b5f3c7216ef0e5270572
Jean-Sébastien Lerat <belegkarnil@169.254.12.4>
2019-05-14 10:42:03 +0200

    in-dev get data (REST API)

--- WebPortal/application/controllers/Rest.php
+++ WebPortal/application/controllers/Rest.php
@@ -64,7 +64,7 @@ class Rest extends CI_Controller {
             $this->response['message']='Cannot access to this data source';
         }else{
             $data = $this->DataSourceModel->getDataSource($source);
-            print_r(list_paragraphs($data['url'],true)[0]['content']);
+            $this->response['body']['data'] = list_paragraphs($data['url'],true)[0]['content'];
         }
         $this->output();
     }
