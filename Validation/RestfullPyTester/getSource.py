33e28f951f66dcebaca05198830592294aa7ea27
Jean-Sébastien Lerat <belegkarnil@169.254.12.4>
2019-05-14 09:21:24 +0200

    Add Python rest api tester scripts

---
+++ Validation/RestfullPyTester/getSource.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+#coding: utf-8
+import requests
+
+WALLSMART_URL	= 'http://192.168.2.168/index.php/Rest/index'
+USERNAME			= 'supernadine'
+PASSWORD			= 'test'
+SOURCE			= '88'
+
+def main(args):
+	global ZEPPELIN_URL, USERNAME, PASSWORD, SOURCE
+	r = requests.post(WALLSMART_URL, json={'username': USERNAME, 'password': PASSWORD, 'source': SOURCE})
+	print("Status code: "+str(r.status_code))
+	print(r.json())
+
+if(__name__ == '__main__'):
+	import sys
+	main(sys.argv)
---
+++ Validation/RestfullPyTester/listSources.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#coding: utf-8
+import requests
+
+WALLSMART_URL	= 'http://192.168.2.168/index.php/Rest/list'
+USERNAME			= 'supernadine'
+PASSWORD			= 'test'
+
+def main(args):
+	global ZEPPELIN_URL, USERNAME, PASSWORD
+	r = requests.post(WALLSMART_URL, json={'username': USERNAME, 'password': PASSWORD})
+	print("Status code: "+str(r.status_code))
+	print(r.json())
+
+if(__name__ == '__main__'):
+	import sys
+	main(sys.argv)
