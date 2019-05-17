31e32112cdebf375b7c4b4759f903cd49b34480b
Jean-Sébastien Lerat <belegkarnil@localhost.belegkarnil-shs>
2019-05-17 09:57:00 +0200

    dev suggestion.js

--- WebPortal/assets/javascript/suggestion.js
+++ WebPortal/assets/javascript/suggestion.js
@@ -1,11 +1,21 @@
+
 var inputsuggestion = $(document).select('#login.suggestion');
 
+var suggestions = document.createElement('datalist');
+suggestions.setAttribute("id", "login_suggestions");
+suggestions.setAttribute("class", "suggestion");
+
+inputsuggestion.insertAdjacentElement('afterend', suggestions)
+
+
+/*
 $('<datalist/>', {
     id: 'login_suggestions',
     class: 'suggestion'
 }).insertAfter('#login.suggestion');
 
 var suggestions = $(document).select('#login_suggestions.suggestion');
+*/
 
 inputsuggestion.attr("list", "login_suggestions");
 
