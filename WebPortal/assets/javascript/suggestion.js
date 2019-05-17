2210c10bba94e5d3267a1dbc159d511b2967f784
Jean-Sébastien Lerat <belegkarnil@localhost.belegkarnil-shs>
2019-05-17 09:49:37 +0200

    dev suggestion.js

--- WebPortal/assets/javascript/suggestion.js
+++ WebPortal/assets/javascript/suggestion.js
@@ -18,7 +18,7 @@ inputsuggestion.bind( "input", function(data) {
 		for(var i=0;i<response.length;i++){
 			content += "<option value='" + response[i] + "'></option>\n";
 		}
-		suggestions.append('');
+		$(document).select('#login_suggestions.suggestion').append(content);
 	});
 });
 
