d160385385663fd26a6a88257e735e85d97c8d63
Jean-Sébastien Lerat <belegkarnil@10.0.0.125>
2019-05-17 05:38:57 +0200

    dev suggestion.js

--- WebPortal/assets/javascript/suggestion.js
+++ WebPortal/assets/javascript/suggestion.js
@@ -1,5 +1,8 @@
 $("#login .suggestion").bind( "input", function(data) {
 	console.log(data);
 });
+$("#login .suggestion").bind( "keypress", function(data) {
+	console.log(data);
+});
 
 console.log($("#login .suggestion"));
