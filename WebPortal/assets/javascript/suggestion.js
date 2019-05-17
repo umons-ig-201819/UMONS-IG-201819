5d54704fbe4a1a89e8e0abc5a89a94997a430874
Jean-Sébastien Lerat <belegkarnil@10.0.0.125>
2019-05-17 06:15:33 +0200

    dev suggestion.js

--- WebPortal/assets/javascript/suggestion.js
+++ WebPortal/assets/javascript/suggestion.js
@@ -1,8 +1,26 @@
+var inputsuggestion = $(document).select('#login.suggestion');
 
-$(document).select('#login.suggestion').bind( "input", function(data) {
-	console.log(data)
+inputsuggestion.bind( "input", function(data) {
+	var elem = data.target;
 	$.get('http://' + window.location.hostname+ "/index.php/search/usersuggestion/10/s", function(response) {
 		console.log(response);
 		console.log("done");
 	});
 });
+
+var suggestions = jQuery('<datalist/>', {
+    id: 'login_suggestions',
+    class: 'suggestion'
+}).insertAfter(inputsuggestion.parent());
+
+console.log(suggestions);
+
+/*
+<datalist id="suggestions">
+<option value="Black">
+<option value="Red">
+<option value="Green">
+<option value="Blue">
+<option value="White">
+</datalist>
+*/
