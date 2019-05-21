b55be2672b16cd0cc2bf493f5b56edd8ff43cc51
louvignies2990 <louvignies2990@hotmail.com>
2019-05-21 02:25:19 +0200

    Update DataSourceModel.php

--- WebPortal/application/models/DataSourceModel.php
+++ WebPortal/application/models/DataSourceModel.php
@@ -722,7 +722,7 @@ class DataSourceModel extends CI_Model{
 				WHERE `uf_id_invite`=$advisorID AND `uf_id_fichier`=$sourceID";
 	    $query = $this->db->query($sql);
 	    $acces=$query->row_array();
-	    if(is_null(intval($acces['access']))) return '4';
+	    if(is_null(intval($acces['access']))) return "4";
 	    return $acces;
 	}
 	/**
@@ -739,7 +739,7 @@ class DataSourceModel extends CI_Model{
 				WHERE `fp_id_projet`=$projectID AND `fp_id_fichier`=$sourceID";
 	    $query = $this->db->query($sql);
 	    $acces=$query->row_array();
-            if(is_null(intval($acces['access']))) return '4';
+            if(is_null(intval($acces['access']))) return "4";
 	    return $acces;
 	}
 }
