ef5d6dc71e6a61ecd9ceeb687e13763ea1c4251a
louvignies2990 <louvignies2990@hotmail.com>
2019-06-08 20:31:20 +0200

    Update UserModelTest.php
    oubli d'une parenthèse

--- Validation/Connection/UserModelTest.php
+++ Validation/Connection/UserModelTest.php
@@ -155,7 +155,7 @@ class UserModelTest extends TestCase{
         $res=$this->userModel->getUsers();
         $this->assertTrue(!is_null($res),true);
         $this->assertTrue(count($res)>9,true);
-        $this->assertEquals($res[0]["id"],intval($lastid);
+        $this->assertEquals($res[0]["id"],intval($lastid));
         $this->assertEquals($res[1]["id"],5);
         $this->assertEquals($res[2]["id"],1);
         $this->assertEquals($res[9]["id"],2);
